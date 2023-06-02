cabf575337e3bb66188a4a5c753ca740ef3b7aa6
thutanyan <thutathuta319@gmail.com>
2023-06-02 17:55:16 +0630

    add swagger

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 // routes
 app.use("/api/v1/auth",authRouter);
 app.use("/api/v1/jobs",authenticatedUser,jobRouter);
